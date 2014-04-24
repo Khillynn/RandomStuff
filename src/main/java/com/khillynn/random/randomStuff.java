28c4d0ccad364cdd22fd843271117bc3b13b89d1
Khillynn <deannabtilley@gmail.com>
2014-04-24 14:14:16 -0400

    Initial

--- src/main/java/com/khillynn/random/randomStuff.java
+++ src/main/java/com/khillynn/random/randomStuff.java
@@ -3,12 +3,15 @@ package com.khillynn.random;
 import com.khillynn.Troll;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -77,6 +80,16 @@ public class randomStuff extends JavaPlugin implements Listener
             }
             if(p.getInventory().getLeggings() != null) {
                 p.getInventory().getBoots().setDurability((short) 0);
+            }
+        }
+    }
+
+    @EventHandler
+    public void PlayerRightClick(PlayerInteractEvent event)
+    {
+        if(event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+            if(event.getItem()!=null && event.getItem().getType()!=null && event.getItem().getType().equals(Material.EMERALD)){
+
             }
         }
     }
