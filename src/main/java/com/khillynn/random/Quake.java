b0f97ea672882740e8ee3915e42bcfbcfb6aa437
Khillynn <deannabtilley@gmail.com>
2014-05-07 14:11:39 -0400

    Initial

--- src/main/java/com/khillynn/random/Quake.java
+++ src/main/java/com/khillynn/random/Quake.java
@@ -1,16 +1,18 @@
 package com.khillynn.random;
 
 
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.WitherSkull;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
@@ -20,23 +22,47 @@ public class Quake
 {
     public class randomStuff extends JavaPlugin implements Listener
     {
+        public void onEnable() {
+            getServer().getPluginManager().registerEvents(this, this);
+        }
+
         @EventHandler
         public void onPlayerSpawn(PlayerRespawnEvent event)
         {
             Player p = event.getPlayer();
-            p.getPlayer().getInventory().setItemInHand(new ItemStack(Material.WOOD_HOE, 1));
+            if(!p.getInventory().contains(Material.WOOD_HOE))
+            {
+                p.getPlayer().getInventory().setItemInHand(new ItemStack(Material.WOOD_HOE, 1));
+            }
         }
 
+        @EventHandler
         public void onInteract(PlayerInteractEvent e) {
             Location loc = e.getPlayer().getLocation();
 
             if(e.getAction() == Action.RIGHT_CLICK_AIR) {
                 if(e.getPlayer().getItemInHand().getType() == Material.WOOD_HOE) {
-                    e.getPlayer().launchProjectile(WitherSkull.class);
-                    e.getPlayer().playSound(loc, Sound.GHAST_FIREBALL, 100, 0);
-
-                    WitherSkull bullet = (WitherSkull) e.getPlayer().getWorld().spawnEntity(e.getPlayer().getLocation(), EntityType.WitherSkull);
+                    WitherSkull bullet = (WitherSkull) e.getPlayer().getWorld().spawnEntity(e.getPlayer().getLocation(), EntityType.WITHER_SKULL);
                     bullet.setVelocity(e.getPlayer().getLocation().getDirection().multiply(1.5));
+                    bullet.getWorld().playEffect(loc, Effect.SMOKE, 0, 50);
+                    e.getPlayer().playSound(loc, Sound.GHAST_FIREBALL, 100, 0);
+                }
+            }
+        }
+
+        @EventHandler
+        public void onEntityDamageByEntityEvent(EntityDamageByEntityEvent e){
+            if(e.getEntity() instanceof WitherSkull)
+            {
+                if(e.getCause() == EntityDamageEvent.DamageCause.PROJECTILE)
+                {
+                    if (e.getDamager() instanceof WitherSkull)
+                    {
+                        if (e.getEntity() instanceof Player)
+                        {
+                            e.setDamage(20);
+                        }
+                    }
                 }
             }
         }
